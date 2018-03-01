9b44a13da4ec3328b6fd1875ae7499a143d9bfc7
Esteem Okoro <est66@bitbucket.org>
2018-03-01 10:08:25 +0100

    Supression  des fields user innutiles

--- models/user.model.js
+++ models/user.model.js
@@ -7,17 +7,6 @@ var userSchema = Schema({
     firstname: { type: String, unique: false, maxlength: 2 - 20 },
     lastname: { type: String, unique: false, maxlength: 2 - 20 },
     role: { type: String, enum: ['citzen', 'manager'] },
-    //ancienne version
-    username: { type: String, unique: false },
-    meta: { // Nested document
-        //deleted: { type: Boolean, default: false },
-        resolved: { type: Boolean, default: false },
-        //Main date of issues
-        creationDate: { type: Date, default: Date.now },
-        updatedDate: { type: Date, default: Date.now },
-        resolvedDate: { type: Date, default: null },
-        //deletedDate: { type: Date, default: null },
-    }
 }, { timestamps: { createdAt: 'createdAt' } });
 
 
